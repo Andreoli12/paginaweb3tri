b188d937b6b2f98ecb9370e61f7615114802af56
Andreoli12 <164929018+Andreoli12@users.noreply.github.com>
2024-11-21 11:23:09 -0300

    s

---
+++ perguntas.js
@@ -0,0 +1,55 @@
+criaCartao(
+    'História',
+    'quem foi o primeiro imperador romano?',
+    'O primeiro imperador romano foi Augusto, que governou de 27 a.C. até sua morte em 14 d.C., após a queda da República Romana e o estabelecimento do Império.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo em área territorial?',
+    'O maior país do mundo é a Rússia, com uma área de cerca de 17,1 milhões de quilômetros quadrados.'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu "Dom Quixote"?',
+    'Dom Quixote foi escrito por Miguel de Cervantes, sendo publicado pela primeira vez em 1605.
+
+'
+)
+
+criaCartao(
+    'Política',
+    'Quem foi o primeiro presidente do Brasil?',
+    'O primeiro presidente do Brasil foi Marechal Deodoro da Fonseca, que assumiu em 1889 após a Proclamação da República.'
+)
+
+criaCartao(
+    'Física',
+    'O que é a Lei da Gravitação Universal?',
+    'A Lei da Gravitação Universal, formulada por Isaac Newton, afirma que todos os corpos no universo se atraem com uma força proporcional às suas massas e inversamente proporcional ao quadrado da distância entre eles.'
+)
+
+criaCartao(
+    'Música',
+    'Quem compôs a famosa ópera "A Flauta Mágica"?',
+    'A ópera "A Flauta Mágica" foi composta por Wolfgang Amadeus Mozart, em 1791.'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é o Teorema de Pitágoras?',
+    'O Teorema de Pitágoras afirma que, em um triângulo retângulo, o quadrado da hipotenusa (o lado oposto ao ângulo reto) é igual à soma dos quadrados dos outros dois lados.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que são os vírus?',
+    ' Os vírus são agentes infecciosos microscópicos que não podem se reproduzir por conta própria e dependem de células hospedeiras para se multiplicar.'
+)
+
+criaCartao(
+    'Direito',
+    'O que é o Estado de Direito?',
+    'O Estado de Direito é um sistema em que todas as pessoas, incluindo governantes, estão sujeitas à lei, garantindo direitos e a justiça.'
+)
